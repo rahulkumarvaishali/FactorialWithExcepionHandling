70870c2ad31500c4f4fc36d42b7eff6811ba5e47
Rahul Kumar <rahulrk00297@gmail.com>
2022-09-27 15:28:32 +0530

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int fact = 1;
+        Console.Write("Enter Number to Find Factorial :");
+
+        try
+        {
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Please Enter Numeric Number");
+        }
+        
+        Console.WriteLine(fact);
+    }
+}
